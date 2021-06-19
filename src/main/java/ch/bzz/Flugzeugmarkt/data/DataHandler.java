fc8c33d74cc1acc2bd88054ed7820e82c2698887
Swissforce <martin.dueppenbecker@gmail.com>
2021-06-19 20:54:54 +0200

    writeJSON() verbessert

--- src/main/java/ch/bzz/Flugzeugmarkt/data/DataHandler.java
+++ src/main/java/ch/bzz/Flugzeugmarkt/data/DataHandler.java
@@ -5,9 +5,12 @@ import ch.bzz.Flugzeugmarkt.model.Flugzeug;
 import ch.bzz.Flugzeugmarkt.model.Hersteller;
 import ch.bzz.Flugzeugmarkt.service.Config;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
@@ -245,7 +248,18 @@ public class DataHandler {
      * Schreibt die Flugzeuge in das JSON
      */
     private static void writeJSON() {
-        //diese Methode funktioniert nicht einmal im Bookshelf
-        //TODO
+        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectWriter objectWriter = objectMapper.writer(new DefaultPrettyPrinter());
+        FileOutputStream fileOutputStream = null;
+        Writer fileWriter;
+
+        String bookPath = Config.getProperty("flugzeugJSON");
+        try {
+            fileOutputStream = new FileOutputStream(bookPath);
+            fileWriter = new BufferedWriter(new OutputStreamWriter(fileOutputStream, StandardCharsets.UTF_8));
+            objectWriter.writeValue(fileWriter, getFlugzeugMap().values());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
